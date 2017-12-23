92b96177aa634b66c2893f8b8a578a6c799cb1ff
tyobaskara <prasetyaajibaskara@gmail.com>
2017-12-23 21:12:21 +0700

    alt

--- src/jsx/component/slider-about.js
+++ src/jsx/component/slider-about.js
@@ -17,11 +17,11 @@ export class SliderAbout extends React.Component {
     return (
       <div className="sliderMe">
         <Slider {...settings}>
-          <div><img src="./assets/images/tyo-7.jpg"/></div>
-          <div><img src="./assets/images/tyo-9.jpg"/></div>
-          <div><img src="./assets/images/tyo-10.jpg"/></div>
-          <div><img src="./assets/images/tyo-12.jpg"/></div>
-          <div><img src="./assets/images/tyo-13.jpg"/></div>
+          <div><img src="./assets/images/tyo-7.jpg" alt="tyo-slider-1"/></div>
+          <div><img src="./assets/images/tyo-9.jpg" alt="tyo-slider-2"/></div>
+          <div><img src="./assets/images/tyo-10.jpg" alt="tyo-slider-3"/></div>
+          <div><img src="./assets/images/tyo-12.jpg" alt="tyo-slider-4"/></div>
+          <div><img src="./assets/images/tyo-13.jpg" alt="tyo-slider-5"/></div>
         </Slider>
       </div>
     );
