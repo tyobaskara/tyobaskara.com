4ed629d23d6a4ed7533edb149b8b803c1a69f4a5
tyobaskara <prasetyaajibaskara@gmail.com>
2017-12-30 02:07:08 +0700

    lightwidget

--- src/jsx/component/instagram-gallery.js
+++ src/jsx/component/instagram-gallery.js
@@ -7,7 +7,7 @@ export const InstagramGallery = () => (
             <h3 className="orgTitle">Follow me on instagram</h3>
         </div>
         <div className="igWrapper" style={{padding: '20px 0'}}>
-            <iframe src="//lightwidget.com/widgets/3718dfafcbac5ec7b993e46fde42e086.html" scrolling="no" allowtransparency="true" className="lightwidget-widget" style={{width: '100%', border: 0, minHeight: '300px', overflow: 'hidden'}}></iframe>
+            <iframe src="//lightwidget.com/widgets/4229ba7325bf5c2bb964e7ecaa4a0f95.html" scrolling="no" allowtransparency="true" className="lightwidget-widget" style={{width: '100%', minHeight: '300px', border: 0,  overflow: 'hidden'}}></iframe>
         </div>
     </div>
 );
